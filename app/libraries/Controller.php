e068cbe753b84ce6dcef202a1fb70c6b9bd58e67
Genaro Martínez <genarotms@gmail.com>
2019-04-21 20:47:40 -0500

    Se agregan modelos iniciales (manejo de rutas y base de datos)

---
+++ app/libraries/Controller.php
@@ -0,0 +1,26 @@
+<?php
+class Controller {
+	/**
+	 * [Carga el modelo]
+	 * @param  [string] $model [Nombre del modelo]
+	 * @return [object] $model [Objeto modelo]
+	 */
+	public function model($model) {
+		require_once "../app/models/{$model}.php";
+		return new $model();
+	}
+
+	/**
+	 * [Carga la vista]
+	 * @param [string] $view [Nombre de la vista]
+	 * @param [array] $data [Datos del controlador]
+	 * @return [Vista html]
+	 */
+	public function view($view, $data = []) {
+		if (file_exists("../app/views/{$view}.php")) {
+			require_once "../app/views/{$view}.php";
+		} else {
+			die('La vista no existe');
+		}
+	}
+}
